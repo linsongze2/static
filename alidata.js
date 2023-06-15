95c40d402489b90d503bd867f06c90cb6fa81339
root <root@debian.debian>
2023-06-15 10:49:12 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：0.5的男人 0.5の男",url:"https://www.aliyundrive.com/s/6Pdbv2dLCqt"},
+const aliData = [{name:"名称：报告合集",url:"https://www.aliyundrive.com/s/2mQyLE4NRdn"},
+{name:"名称：0.5的男人 0.5の男",url:"https://www.aliyundrive.com/s/6Pdbv2dLCqt"},
 {name:"名称：Internet Download Manager (IDM) 6.41.15 学习版",url:"https://www.aliyundrive.com/s/HdtyPS4e17j"},
 {name:"名称：公诉 4K更32",url:"https://www.aliyundrive.com/s/owLvC2Xh1KU"},
 {name:"名称：【共9册】 未读科幻经典升级版 | 电子书籍",url:"https://www.aliyundrive.com/s/yUKjws1VsiD"},
