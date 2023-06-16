543ca5a07ed36e0684d6c803850055c310c099ce
root <root@debian.debian>
2023-06-16 12:33:13 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：惊天营救2(2023) 4K HDR 中字内嵌字幕",url:"https://www.aliyundrive.com/s/Di9Fpd85HsZ"},
+const aliData = [{name:"名称：【岂牛学堂】保险实战训练营",url:"https://www.aliyundrive.com/s/hdzq9BohgcH"},
+{name:"名称：汪汪队立大功 国语 1-9季 新增某酷4K 第9季",url:"https://www.aliyundrive.com/s/BBLqimoFJyv"},
+{name:"名称：惊天营救2(2023) 4K HDR 中字内嵌字幕",url:"https://www.aliyundrive.com/s/Di9Fpd85HsZ"},
 {name:"名称：【唐奇财税】金珊老师：《老板财税必修课》",url:"https://www.aliyundrive.com/s/pDGYmA4Gjmu"},
 {name:"名称：【百战程序员】Java互联网架构师",url:"https://www.aliyundrive.com/s/QY2kut6bYwy"},
 {name:"名称：剧集更新",url:"https://www.aliyundrive.com/s/VXzw7YJrzch"},
