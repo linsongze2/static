2cee5b315f16faf3098c4951c47708a02bc4afc4
root <root@debian.debian>
2023-06-16 00:33:12 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：坎大哈陷落 Kandahar (2023)",url:"https://www.aliyundrive.com/s/uTifG4Dq8tu"},
+const aliData = [{name:"名称：《我们的父辈》【1080P】【双版本】【德语·内封外挂字幕】【豆瓣9.6分】",url:"https://www.aliyundrive.com/s/D9Y9JWsPSj5"},
+{name:"名称：坎大哈陷落 Kandahar (2023)",url:"https://www.aliyundrive.com/s/uTifG4Dq8tu"},
 {name:"名称：深度改变 | 电子书籍",url:"https://www.aliyundrive.com/s/pvUcH4U2jTn"},
 {name:"名称：完美世界 更新115",url:"https://www.aliyundrive.com/s/m5LswcfxG6B"},
 {name:"名称：这么多年 (2023) 2160p 高码率 &amp; 4K 杜比视界 【洗码版易和谐】",url:"https://www.aliyundrive.com/s/ApKLBgRzb2G"},
