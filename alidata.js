efb3f9462ecd0146c4ec9d80028adac29edb5467
root <root@debian.debian>
2023-06-18 09:41:15 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：欢迎来到王之国 King The Land 2023",url:"https://www.aliyundrive.com/s/4YCZi1nQ9oA"},
+const aliData = [{name:"名称：90天B端产品经理实战班",url:"https://www.aliyundrive.com/s/GCXSuxbvsK9"},
+{name:"名称：欢迎来到王之国 King The Land 2023",url:"https://www.aliyundrive.com/s/4YCZi1nQ9oA"},
 {name:"名称：印象派：绘画与革命 S1(2011) 4K SDR 中英外挂字幕",url:"https://www.aliyundrive.com/s/HzrdXQy2yRn"},
 {name:"名称：指环王 三部曲 加长版 110GB/部 刮削",url:"https://www.aliyundrive.com/s/tiSCBUtCiRk"},
 {name:"名称：梦魇绝镇 第二季 From (2023) 更9 美剧 科幻 恐怖",url:"https://www.aliyundrive.com/s/f9xWY6u3eaF"},
