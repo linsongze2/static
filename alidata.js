283d5de020eab918a2c8754e95319dda7edc38b5
root <root@debian.debian>
2023-06-21 10:17:13 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：最佳出价 The best Offer (2013)",url:"https://www.aliyundrive.com/s/JdCwxDSWWCr"},
+const aliData = [{name:"名称：MySQL提升课程 全面讲解MySQL架构设计",url:"https://www.aliyundrive.com/s/Ec3whojaoFv"},
+{name:"名称：最佳出价 The best Offer (2013)",url:"https://www.aliyundrive.com/s/JdCwxDSWWCr"},
 {name:"名称：做工的人 (2023) 台湾喜剧",url:"https://www.aliyundrive.com/s/inqSELKk5JB"},
 {name:"名称：双面线索 Gumraah (2023)",url:"https://www.aliyundrive.com/s/FyZxbN3jyCy"},
 {name:"名称：RocketMQ核心技术精讲与高并发抗压实战",url:"https://www.aliyundrive.com/s/pFsZ1r3HXw9"},
