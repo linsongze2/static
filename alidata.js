55443fa021c6a4810fa41d84bfb23947f8b109a5
root <root@debian.debian>
2023-06-26 01:57:14 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：信息系统项目管理师教程 (第四版) 电子书籍",url:"https://www.aliyundrive.com/s/nuhRAVJppDz"},
+const aliData = [{name:"名称：Python爬虫高级开发工程师",url:"https://www.aliyundrive.com/s/Mi8J5LmwEB8"},
+{name:"名称：信息系统项目管理师教程 (第四版) 电子书籍",url:"https://www.aliyundrive.com/s/nuhRAVJppDz"},
 {name:"名称：【高码第2部】保你平安[高码版][国语配音/中文字幕].Post.Truth.2022.WEB-DL.4K.HQ.H.265.10bit.AAC.5.1-CTRLTV 15.69GB",url:"https://www.aliyundrive.com/s/s4nd56eH9ZH"},
 {name:"名称：恶鬼 Revenant(迪士尼官方字幕附多版本）",url:"https://www.aliyundrive.com/s/dSun9ieLUdA"},
 {name:"名称：【高码第3部】倍儿喜欢你[高码版][国语配音/中文字幕].I.Like.You.More.2023.2160p.HQ.WEB-DL.H265.10bit.DDP2.0-GPTHD 14.07GB",url:"https://www.aliyundrive.com/s/2beaLd8FVa6"},
