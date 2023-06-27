4764cf99a4ac8a335a046695e6e47ffbc9034bcc
root <root@debian.debian>
2023-06-27 08:09:15 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：梦魇绝镇 (2023) S02 2160p &amp; 1080p 高码率 内封中英双语 【10集完结】",url:"https://www.aliyundrive.com/s/MLMGkQ8UbVL"},
+const aliData = [{name:"名称：雪鹰领主 (2023) 1080/4K 更16 古力娜扎",url:"https://www.aliyundrive.com/s/HoZrpwY7zju"},
+{name:"名称：闪耀的她 (2023) 1080/4K HDR 更22 秦岚",url:"https://www.aliyundrive.com/s/336uZFAW2hJ"},
+{name:"名称：梦魇绝镇 (2023) S02 2160p &amp; 1080p 高码率 内封中英双语 【10集完结】",url:"https://www.aliyundrive.com/s/MLMGkQ8UbVL"},
 {name:"名称：圣斗士星矢 真人版 (2023) 官方中字",url:"https://www.aliyundrive.com/s/Mn2jqNumN9c"},
 {name:"名称：圣斗士星矢 真人版 (2023) ＋圣斗士星矢 系列大合集",url:"https://www.aliyundrive.com/s/Ctg7T4fChj7"},
 {name:"名称：毒舌律师 / 毒舌大状 (2023) 2160p 高码 国粤多音轨 DTS &amp; DDP 5.1环绕 + 1080p 高码 内封双语 [已刮削]",url:"https://www.aliyundrive.com/s/GC2LxFQ1uKc"},
