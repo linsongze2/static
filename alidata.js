a9df955c79d503758c3937b7533be8682227df43
root <root@debian.debian>
2023-06-28 07:41:26 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：手机版《死亡笔记》？丨网飞惊悚新剧《永久删除》全8集",url:"https://www.aliyundrive.com/s/FNvQVWtcPYE"},
+const aliData = [{name:"名称：清潭国际高中 청담제국고등학교 (2023) 10集完结",url:"https://www.aliyundrive.com/s/xzyKBmhRM65"},
+{name:"名称：手机版《死亡笔记》？丨网飞惊悚新剧《永久删除》全8集",url:"https://www.aliyundrive.com/s/FNvQVWtcPYE"},
 {name:"名称：劫机七小时 Hijack (2023) 更2",url:"https://www.aliyundrive.com/s/N6WKGWj3G7t"},
 {name:"名称：女子推理社 (2023) 11集下 戚薇/张雨绮",url:"https://www.aliyundrive.com/s/yTKVgKaWSdm"},
 {name:"名称：怦然心动20岁 第三季 (2023) 更0628",url:"https://www.aliyundrive.com/s/K7Vxx5iUNVP"},
