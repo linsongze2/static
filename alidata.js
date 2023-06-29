9d9701b6e0b8d2e186df8c8e5c23f519eca63921
root <root@debian.debian>
2023-06-29 07:33:18 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：长风渡 更新21-22集 速度保存 明天河蟹",url:"https://www.aliyundrive.com/s/8zyMt7GC73b"},
+const aliData = [{name:"名称：闪耀的她 (2023) 1080/4K HDR 32集完结 秦岚",url:"https://www.aliyundrive.com/s/336uZFAW2hJ"},
+{name:"名称：长风渡 更新21-22集 速度保存 明天河蟹",url:"https://www.aliyundrive.com/s/8zyMt7GC73b"},
 {name:"名称：偷偷藏不住 [2023] 4k60帧更新16集",url:"https://www.aliyundrive.com/s/PyACT5WxRAV"},
 {name:"名称：猎魔人 第三季 The Witcher Season 3 (2023) 上部5集完结 猎魔人终极之战",url:"https://www.aliyundrive.com/s/357MzaSPNzw"},
 {name:"名称：与你十年予我半生 (2023) 24集完结",url:"https://www.aliyundrive.com/s/wL3EvwYUrh8"},
