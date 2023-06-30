5b500bee37f527a6b1f7b56e1222c2a0d9ecf3c9
root <root@debian.debian>
2023-06-30 06:33:14 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：乘风 2023 （更6月30日）",url:"https://www.aliyundrive.com/s/cyaLusuTsJY"},
+const aliData = [{name:"名称：柏拉图式关系 Platonic (2023)",url:"https://www.aliyundrive.com/s/AXf7gGRHEPn"},
+{name:"名称：乘风 2023 （更6月30日）",url:"https://www.aliyundrive.com/s/cyaLusuTsJY"},
 {name:"名称：当我飞奔向你 (2023) 1080/4K HDR 24集完结",url:"https://www.aliyundrive.com/s/4WXncR1pZw7"},
 {name:"名称：人生路不熟 正式版 多版本合集",url:"https://www.aliyundrive.com/s/GRxvD6YP5g7"},
 {name:"名称：无辣不欢 (2023) 12集全",url:"https://www.aliyundrive.com/s/5Lp4qvT4Bgj"},
