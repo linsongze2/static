6e6485b43718c174d1cf0a95cc3f3fdd896c6681
root <root@debian.debian>
2023-06-30 23:49:14 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：羊毛战记 Silo (2023) 2160p HDR + 4K 杜比视界 + 1080p 高码 + 内封双语版本(4K HDR / 1080p) 【S01完结】",url:"https://www.aliyundrive.com/s/n71kWdHPHxU"},
+const aliData = [{name:"名称：豆瓣9.3,最伟大的教育片之一,一切源于纯粹的爱 《放牛班的春天》",url:"https://www.aliyundrive.com/s/ncg7PzbwRMs"},
+{name:"名称：魔法奇缘2：解除魔法 2022年11月份最新奇幻冒险电影",url:"https://www.aliyundrive.com/s/ZYijZustAUR"},
+{name:"名称：羊毛战记 Silo (2023) 2160p HDR + 4K 杜比视界 + 1080p 高码 + 内封双语版本(4K HDR / 1080p) 【S01完结】",url:"https://www.aliyundrive.com/s/n71kWdHPHxU"},
 {name:"名称：【和谐补档】 速度与激情10 (2023) 2160p HDR &amp; DV / 1080p 高码率 内封简英 &amp; 繁英双语 【人工精校双语】",url:"https://www.aliyundrive.com/s/jTpW3BtNTSs"},
 {name:"名称：天赐的声音 第四季 (2023) 更0630",url:"https://www.aliyundrive.com/s/Et9MRMyFY8g"},
 {name:"名称：斗罗大陆2：绝世唐门 (2023) 更3",url:"https://www.aliyundrive.com/s/jdzUbemRiNx"},
