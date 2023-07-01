3ff84b526948ca1b83ef3b4befb237bffa7aa72b
root <root@debian.debian>
2023-06-30 21:09:13 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：【全5册】 索恩·人物档案馆：在历史书写中思考人类命运 | 电子书籍",url:"https://www.aliyundrive.com/s/n5VkkiJBVYZ"},
+const aliData = [{name:"名称：给孩子的高效学习手册（漫画版）",url:"https://www.aliyundrive.com/s/viH6rCPrZ3U"},
+{name:"名称：【全5册】 索恩·人物档案馆：在历史书写中思考人类命运 | 电子书籍",url:"https://www.aliyundrive.com/s/n5VkkiJBVYZ"},
 {name:"名称：暗夜行者",url:"https://www.aliyundrive.com/s/1DkZ2VqGeeX"},
 {name:"名称：【开课吧】AI算法落地与工程部署实战",url:"https://www.aliyundrive.com/s/oLDndY9qqb6"},
 {name:"名称：【光影魔方】商业PS后期课进阶班课程",url:"https://www.aliyundrive.com/s/c5optAyckn8"},
