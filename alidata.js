b301c610dca86f50abba87482998e216f7d310a8
root <root@debian.debian>
2023-07-03 05:41:30 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：【共7册】 日本·军鉴系列 (001-007) 电子书籍",url:"https://www.aliyundrive.com/s/mdSaWRymHFR"},
+const aliData = [{name:"名称：Google老师亲授 TensorFlow2.0 入门到进阶",url:"https://www.aliyundrive.com/s/rwdbSWokvDd"},
+{name:"名称：【共7册】 日本·军鉴系列 (001-007) 电子书籍",url:"https://www.aliyundrive.com/s/mdSaWRymHFR"},
 {name:"名称：今生也请多指教 이번 생도 잘 부탁해 (2023) 更6",url:"https://www.aliyundrive.com/s/fQxprnpqN8w"},
 {name:"名称：零基础玩转Linux+Ubuntu实战视频课程",url:"https://www.aliyundrive.com/s/dMx94LCSK4x"},
 {name:"名称：欢迎来到王之国 킹더랜드 (2023) 更6 李俊昊/林允儿",url:"https://www.aliyundrive.com/s/ZGtWBgdwEVj"},
