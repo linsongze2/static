d6f99e8fe6da59f050eb778bf53cee54acebf836
root <root@debian.debian>
2023-07-05 09:49:13 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：剧集更新",url:"https://www.aliyundrive.com/s/GCj9pfog9rf"},
+const aliData = [{name:"名称：咒术回战 第二季2023持续更新中",url:"https://www.aliyundrive.com/s/3LT3vb2UjVm"},
+{name:"名称：剧集更新",url:"https://www.aliyundrive.com/s/GCj9pfog9rf"},
 {name:"名称：心跳 가슴이뛴다 (2023) 韩剧 更E03",url:"https://www.aliyundrive.com/s/YzhU4bG3os7"},
 {name:"名称：犯罪都市3 (2023) 1080p 高码率 无黑边 内封简 / 繁 / 英 【人工听译字幕OCR内封】【附1-2】",url:"https://www.aliyundrive.com/s/M2En9dWeyk1"},
 {name:"名称：偷偷藏不住 [2023] 4k60fps 更新21",url:"https://www.aliyundrive.com/s/vaEFZSMYCvN"},
