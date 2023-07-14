cf77f51e5723377a21155c0a5990f4e07e3a380f
root <root@debian.debian>
2023-07-14 06:09:19 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：【宁静】2023淘宝数据化运营14式",url:"https://www.aliyundrive.com/s/tHFuEYSb3rh"},
+const aliData = [{name:"名称：《阿里云盘小白羊》",url:"https://www.aliyundrive.com/s/cUV1xBdQdfN"},
+{name:"名称：【宁静】2023淘宝数据化运营14式",url:"https://www.aliyundrive.com/s/tHFuEYSb3rh"},
 {name:"名称：【4K】2023 《基地第二季第 1 集 Foundation S02E01》Apple TV+ 真人科幻系列剧集",url:"https://www.aliyundrive.com/s/3EFxVKZb9pz"},
 {name:"名称：星际穿越",url:"https://www.aliyundrive.com/s/b3XnWSNLTXt"},
 {name:"名称：武工队传奇 (2013)",url:"https://www.aliyundrive.com/s/p1mxBfpewJj"},
