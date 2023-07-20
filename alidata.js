ddd893d78ffd46f706512df282c5cd21bc7a7cb4
root <root@debian.debian>
2023-07-20 10:41:28 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：金爆行动 Operation Fortune: Rue de guerre (2023) 杰森·斯坦森",url:"https://www.aliyundrive.com/s/DmN6omZcw3F"},
+const aliData = [{name:"名称：2023抖店·起店新玩法，店铺基础搭建，选类目和单品的方法，单品打造模式",url:"https://www.aliyundrive.com/s/xufffsGzFw9"},
+{name:"名称：金爆行动 Operation Fortune: Rue de guerre (2023) 杰森·斯坦森",url:"https://www.aliyundrive.com/s/DmN6omZcw3F"},
 {name:"名称：郎君不如意/吴宣仪 陈哲远/奇幻 喜剧",url:"https://www.aliyundrive.com/s/2XKaznctZpE"},
 {name:"名称：不完美受害人 (2023) 1080/4K",url:"https://www.aliyundrive.com/s/ojMNZmBbUu1"},
 {name:"名称：永生",url:"https://www.aliyundrive.com/s/TuJeKyEW8T7"},
