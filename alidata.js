ecce88773b84a6b1936ed20746580605f6e29a8d
root <root@debian.debian>
2023-07-21 03:09:15 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：赶尽杀绝(2007) Remux 内封中字",url:"https://www.aliyundrive.com/s/PyHzwHJtrvs"},
+const aliData = [{name:"名称：最后的武士(2003) Remux 外挂中字",url:"https://www.aliyundrive.com/s/7VVk4WpTG34"},
+{name:"名称：赛末点(2005) 内封中字",url:"https://www.aliyundrive.com/s/LVUQGMUXitF"},
+{name:"名称：赶尽杀绝(2007) Remux 内封中字",url:"https://www.aliyundrive.com/s/PyHzwHJtrvs"},
 {name:"名称：通天塔(2006) REMUX 外挂中字",url:"https://www.aliyundrive.com/s/Xz8ohpDD6DQ"},
 {name:"名称：活着 VIVANT (2023) 高分日剧 1080p 日语中字 更E01",url:"https://www.aliyundrive.com/s/ayitpsaDrjc"},
 {name:"名称：一级重罪(2002) REMUX 外挂中字",url:"https://www.aliyundrive.com/s/8ospuywNnH8"},
