18031ecc6fecef401274978e1caa023547c5113c
root <root@debian.debian>
2023-07-22 22:41:30 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：名侦探柯南/名探偵コナン 更至1091集即1149",url:"https://www.aliyundrive.com/s/M3V5U6UB9xX"},
+const aliData = [{name:"名称：斗破苍穹 年番 (2022) 4K 同步更新中",url:"https://www.aliyundrive.com/s/MWZjyj8vJTk"},
+{name:"名称：名侦探柯南/名探偵コナン 更至1091集即1149",url:"https://www.aliyundrive.com/s/M3V5U6UB9xX"},
 {name:"名称：tiktok投流实操课",url:"https://www.aliyundrive.com/s/yr5SC2hLUSh"},
 {name:"名称：囚犯之女 Prisoner's Daughter（2023）",url:"https://www.aliyundrive.com/s/Xp11QBuiTyk"},
 {name:"名称：闲鱼大流量联盟玩法 - 2期",url:"https://www.aliyundrive.com/s/bDoKeqTbgrB"},
