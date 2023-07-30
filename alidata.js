257d3359a4750789a49658270ff6a5e215b426d5
root <root@debian.debian>
2023-07-29 23:49:15 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：Numbers：大厦森林的监视者们 (2023) 12集完结",url:"https://www.aliyundrive.com/s/r252ngDT8yW"},
+const aliData = [{name:"名称：镇魂街第三季(2023)[附1-2]",url:"https://www.aliyundrive.com/s/AsBx38GyaRn"},
+{name:"名称：航海王One Piece",url:"https://www.aliyundrive.com/s/gu99XaVwYmP"},
+{name:"名称：Numbers：大厦森林的监视者们 (2023) 12集完结",url:"https://www.aliyundrive.com/s/r252ngDT8yW"},
 {name:"名称：《自由摄影师Plus》全套课程 网易课堂售价3298",url:"https://www.aliyundrive.com/s/pbzfXNykUhK"},
 {name:"名称：铃芽之旅",url:"https://www.aliyundrive.com/s/6HT7WhhD6j6"},
 {name:"名称：斗破苍穹 年番 (2022) 4K 更新55集",url:"https://www.aliyundrive.com/s/MWZjyj8vJTk"},
