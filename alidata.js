7502b5c1d5a247c50770c22e8053afd2c108bde5
root <root@debian.debian>
2023-07-30 14:09:15 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：欢迎来到王之国 (2023) 林允儿-韩剧 更E14",url:"https://www.aliyundrive.com/s/RCaGpVzibvK"},
+const aliData = [{name:"名称：【奈学科技】Java资深研发工程师02期",url:"https://www.aliyundrive.com/s/hrf14YHTTkB"},
+{name:"名称：欢迎来到王之国 (2023) 林允儿-韩剧 更E14",url:"https://www.aliyundrive.com/s/RCaGpVzibvK"},
 {name:"名称：恐惧暗夜 Fear the Night (2023) 1080P",url:"https://www.aliyundrive.com/s/nVFphLgfbyE"},
 {name:"名称：猎魔人 S03 1080p NF 高码率 DDP5.1 Atmos 内封简英 &amp; 繁英双语 附前两季高码内封多国语",url:"https://www.aliyundrive.com/s/xs8NSaEFq5i"},
 {name:"名称：拥挤的房间 / 24个比利 (2023) S01 2160p HDR &amp; 1080p 高码 DDP5.1 外挂双语【悬疑新剧】【完结篇 已刮削】",url:"https://www.aliyundrive.com/s/1gQ4w2vmVUN"},
