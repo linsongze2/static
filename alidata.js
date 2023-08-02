8bcf9721351c0cf7799fbc4509de8b47f377b5af
root <root@debian.debian>
2023-08-02 09:25:20 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：毒舌律师 蓝光原盘REMUX [内封简、繁中字]",url:"https://www.aliyundrive.com/s/u8yQEk2tYAZ"},
+const aliData = [{name:"名称：零基础学习PS(2021-2022)软件",url:"https://www.aliyundrive.com/s/Xh9rxdrgdgg"},
+{name:"名称：好望角-11天视频号变现训练营课程",url:"https://www.aliyundrive.com/s/EPy2ZSQVJRa"},
+{name:"名称：毒舌律师 蓝光原盘REMUX [内封简、繁中字]",url:"https://www.aliyundrive.com/s/u8yQEk2tYAZ"},
 {name:"名称：莲花楼(2023) 4K/HDR 更新20",url:"https://www.aliyundrive.com/s/fh2L83Bw7r3"},
 {name:"名称：顶尖销售的18大基本功教程",url:"https://www.aliyundrive.com/s/Sgz7y74Rvsa"},
 {name:"名称：密室大逃脱 第五季 (2023) 更0802",url:"https://www.aliyundrive.com/s/hRV1iSVNxaV"},
