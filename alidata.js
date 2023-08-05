679ff7b4db3fd0aba4ccb72b6740f13ae4265981
root <root@debian.debian>
2023-08-05 08:25:14 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：拼多多名师线上领跑28天课程",url:"https://www.aliyundrive.com/s/mi1PZN1qKh4"},
+const aliData = [{name:"名称：Java从单体到微服务打造房产销售平台",url:"https://www.aliyundrive.com/s/pSg6jnpV4EB"},
+{name:"名称：拼多多名师线上领跑28天课程",url:"https://www.aliyundrive.com/s/mi1PZN1qKh4"},
 {name:"名称：西班牙语-Español-Spanish",url:"https://www.aliyundrive.com/s/r8qj2aJpSAQ"},
 {name:"名称：不完美受害人",url:"https://www.aliyundrive.com/s/ACyrtjdUMUp"},
 {name:"名称：潜行者",url:"https://www.aliyundrive.com/s/C5aofrQHhpR"},
