a62ad3b64dd11ab7f788df68060333a43b1f7a65
root <root@debian.debian>
2023-08-06 09:49:24 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：驱魔警察 蓝光原盘REMUX [高码英版 官方画质修复] [国粤双语] [内封简、繁中字]",url:"https://www.aliyundrive.com/s/G2HhjfbyuqH"},
+const aliData = [{name:"名称：集体追杀 The Collective（2023）",url:"https://www.aliyundrive.com/s/Cb3iHgSV9bJ"},
+{name:"名称：驱魔警察 蓝光原盘REMUX [高码英版 官方画质修复] [国粤双语] [内封简、繁中字]",url:"https://www.aliyundrive.com/s/G2HhjfbyuqH"},
 {name:"名称：请别相信她 4K高码率 [60帧率版]",url:"https://www.aliyundrive.com/s/PvYeQRb1tYo"},
 {name:"名称：酷我音乐车机 4.8.16 免登陆 解锁VIP",url:"https://www.aliyundrive.com/s/FvfFxUNC4aW"},
 {name:"名称：【乐字节】Java高薪进阶班",url:"https://www.aliyundrive.com/s/4byChbqpeBw"},
