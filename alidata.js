1e9aba0f387203a7100ab979f814737ee1abb82a
root <root@debian.debian>
2023-08-12 06:25:15 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：乐队的夏天 第三季 (2023)",url:"https://www.aliyundrive.com/s/3NfGpfUYDwJ"},
+const aliData = [{name:"名称：裁缝师之家 Terzi (2023) 两季全✨【1080p.SDR】【原轨.高画质】1.6G/集",url:"https://www.aliyundrive.com/s/Bz86zDGTyfd"},
+{name:"名称：乐队的夏天 第三季 (2023)",url:"https://www.aliyundrive.com/s/3NfGpfUYDwJ"},
 {name:"名称：Office 2016 VOL 语言包 免费下载 包含中文、英文、日文、韩文、法文、德文和意大利语等 40 种语言",url:"https://www.aliyundrive.com/s/4dapMQAnUCn"},
 {name:"名称：Excel实战特训班 玩转Excel函数与公式 - 2期",url:"https://www.aliyundrive.com/s/SWEgBkYvqfU"},
 {name:"名称：斗罗大陆第二部",url:"https://www.aliyundrive.com/s/EnwnntCtArv"},
