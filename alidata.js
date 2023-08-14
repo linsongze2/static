2ee4b29be262fbaf98f4bafc8552daef1e591541
root <root@debian.debian>
2023-08-14 07:01:14 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：乐队的夏天 第三季 (2023)",url:"https://www.aliyundrive.com/s/iEzhavKL5ui"},
+const aliData = [{name:"名称：七时吉祥 4K60帧 更新14集",url:"https://www.aliyundrive.com/s/VaSJUcteKer"},
+{name:"名称：长相思 (2023) 1080/4K HDR 更32",url:"https://www.aliyundrive.com/s/je2VSSjUct8"},
+{name:"名称：乐队的夏天 第三季 (2023)",url:"https://www.aliyundrive.com/s/iEzhavKL5ui"},
 {name:"名称：流沙2023",url:"https://www.aliyundrive.com/s/PjggUt4yLXC"},
 {name:"名称：咒中人 คน มนตร์ เวท (2023) 4集完结 泰剧",url:"https://www.aliyundrive.com/s/xLZ5AUAw8uk"},
 {name:"名称：不完美受害人 (2023)✨【2160p.HDR】【原轨.高码率】 3.9G/集",url:"https://www.aliyundrive.com/s/pvtuVbMMdZ8"},
