9bb0760bbd7e986f69f85dba02d37fc3104aeaab
root <root@debian.debian>
2023-08-16 04:09:14 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：超异能族 1080p [更新至09集]",url:"https://www.aliyundrive.com/s/ZeratCqvMfh"},
+const aliData = [{name:"名称：小角色 4KHDR60FPS",url:"https://www.aliyundrive.com/s/M5vgyB4Tfsz"},
+{name:"名称：超异能族 1080p [更新至09集]",url:"https://www.aliyundrive.com/s/ZeratCqvMfh"},
 {name:"名称：偶像漩涡 The Idol (2023) 5集完结 美剧",url:"https://www.aliyundrive.com/s/Y3S5G7WA6Xr"},
 {name:"名称：新特警判官【蓝光原盘+AV1高压、字幕烧录】",url:"https://www.aliyundrive.com/s/4aoyxpyFcso"},
 {name:"名称：从天气项目入门Spring Cloud微服务治理",url:"https://www.aliyundrive.com/s/3YqWXqbQwQZ"},
