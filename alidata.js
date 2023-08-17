7837b139b6a8eab7291e0d41726041ce3934ce3b
root <root@debian.debian>
2023-08-17 07:41:15 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：她的城 [更新至06集]",url:"https://www.aliyundrive.com/s/eFN6HZNbseb"},
+const aliData = [{name:"名称：她的城 4KHDR60FPS 首发6集",url:"https://www.aliyundrive.com/s/heU9pKF14tt"},
+{name:"名称：她的城 [更新至06集]",url:"https://www.aliyundrive.com/s/eFN6HZNbseb"},
 {name:"名称：AI绘画图像生成 Fooocus - 开源免费",url:"https://www.aliyundrive.com/s/if8qrRGi5gE"},
 {name:"名称：Spring Boot2.0深度实践 核心原理拆解+源码分析",url:"https://www.aliyundrive.com/s/ksaNEqVNUAd"},
 {name:"名称：国家中小学智慧教育平台电子教材全套",url:"https://www.aliyundrive.com/s/YqVZ3yM2Fcu"},
