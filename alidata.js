a2301866c641a55c151d687b571e22899704ae90
root <root@debian.debian>
2023-08-17 09:09:14 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：切尔诺贝利 (2019) 4K REMUX【蓝光原盘REMUX+AV1高压、中英原盘字幕烧录】",url:"https://www.aliyundrive.com/s/c5y9tKY82dN"},
+const aliData = [{name:"名称：Excel实战特训班 相见恨晚的100个Excel函数公式 - 8期",url:"https://www.aliyundrive.com/s/MsCgpF3qDrr"},
+{name:"名称：切尔诺贝利 (2019) 4K REMUX【蓝光原盘REMUX+AV1高压、中英原盘字幕烧录】",url:"https://www.aliyundrive.com/s/c5y9tKY82dN"},
 {name:"名称：沧元图 更新至10集",url:"https://www.aliyundrive.com/s/S4Xa7SCfJ2g"},
 {name:"名称：没用的谎言 (2023) 热播韩剧 1080p 内封简繁 更E06【金所泫 / 黄旼炫】",url:"https://www.aliyundrive.com/s/u6BwSzGhzA3"},
 {name:"名称：她的城 4KHDR60FPS 首发6集",url:"https://www.aliyundrive.com/s/heU9pKF14tt"},
