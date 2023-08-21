f33b66e0d7c849944bddb5c20da42c42ffdcd9be
root <root@debian.debian>
2023-08-21 11:41:19 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：这才是心理学-看穿伪心理学的本质",url:"https://www.aliyundrive.com/s/HRLCWesNs8z"},
+const aliData = [{name:"名称：【黑马程序员】解密JVM虚拟机底层原理",url:"https://www.aliyundrive.com/s/tsqudrXTtVb"},
+{name:"名称：这才是心理学-看穿伪心理学的本质",url:"https://www.aliyundrive.com/s/HRLCWesNs8z"},
 {name:"名称：偏见的本质（豆瓣8.2）",url:"https://www.aliyundrive.com/s/cTLaWnFD47P"},
 {name:"名称：100万个约定 一百万个约定 (2023) 丁禹兮 / 杨超越",url:"https://www.aliyundrive.com/s/2AFFeMEvJVe"},
 {name:"名称：武神主宰",url:"https://www.aliyundrive.com/s/9e75UXsdJmw"},
