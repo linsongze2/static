c5f316aed510a19b8da81a0b0afc9fc150f283fb
root <root@debian.debian>
2023-08-23 08:17:14 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：七时吉祥 S01E01~E24 4K",url:"https://www.aliyundrive.com/s/BVbp7Vk2cdE"},
+const aliData = [{name:"名称：七时吉祥 (2023) 4KHDR60FPS 更新24集",url:"https://www.aliyundrive.com/s/GA3KEmMhXBD"},
+{name:"名称：七时吉祥 S01E01~E24 4K",url:"https://www.aliyundrive.com/s/BVbp7Vk2cdE"},
 {name:"名称：现在就出发(2023) S00E04(第二期加更) 4K",url:"https://www.aliyundrive.com/s/cU9rNEAYnEQ"},
 {name:"名称：灼灼风流(2023) S01E01~E14 4K",url:"https://www.aliyundrive.com/s/BPr3xHynuk4"},
 {name:"名称：她的城(2023) S01E01~E18 4K",url:"https://www.aliyundrive.com/s/hVmmyPLBrpk"},
