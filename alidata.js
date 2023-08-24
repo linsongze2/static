9ee9c6d075c372b3b6e0d98c82d57a05be13d1fa
root <root@debian.debian>
2023-08-24 08:09:15 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：诸神的黄昏 S01~S03 Ragnarok (2023)",url:"https://www.aliyundrive.com/s/7fuZ6u1QE57"},
+const aliData = [{name:"名称：入侵 第二季 Invasion Season 2 (2023) [更新至01集][内附S01全季]",url:"https://www.aliyundrive.com/s/KJKUug2KZ6n"},
+{name:"名称：诸神的黄昏 S01~S03 Ragnarok (2023)",url:"https://www.aliyundrive.com/s/7fuZ6u1QE57"},
 {name:"名称：月亮女孩与恶魔恐龙 Marvel’s Moon Girl and Devil Dinosaur (2023)",url:"https://www.aliyundrive.com/s/GHpAabnL4vD"},
 {name:"名称：百变艾琳 Who Is Erin Carter (2023)",url:"https://www.aliyundrive.com/s/dRgLgd2cEjW"},
 {name:"名称：破毒强人 破毒強人 (2023) 1080/4K HDR 30集完结",url:"https://www.aliyundrive.com/s/GtUU5kbKekE"},
