db94e6a33c62cf5cd1cff97118d21e1bc9145465
root <root@debian.debian>
2023-08-24 04:17:16 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：卑劣的街头(2006) 1080P 内封简繁",url:"https://www.aliyundrive.com/s/kSBQhLrpjaP"},
+const aliData = [{name:"名称：【马士兵VIP课程】Java高级互联网架构师【马士兵教育】精英一班",url:"https://www.aliyundrive.com/s/VWZprkdhttA"},
+{name:"名称：卑劣的街头(2006) 1080P 内封简繁",url:"https://www.aliyundrive.com/s/kSBQhLrpjaP"},
 {name:"名称：微淼 - 财务报表分析技能课",url:"https://www.aliyundrive.com/s/YDfo7NDsisz"},
 {name:"名称：高楼惊魂(2012) 1080p 内封简繁",url:"https://www.aliyundrive.com/s/ULpadHAvvz9"},
 {name:"名称：陌生人(2023) 1080p 内封简繁",url:"https://www.aliyundrive.com/s/q1JCP8EBB65"},
