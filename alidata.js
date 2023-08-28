a370606f1e2983b964206f0e8753ae0c8c5e20ae
root <root@debian.debian>
2023-08-28 12:33:16 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：无与伦比的美丽 [更新至02集]",url:"https://www.aliyundrive.com/s/FBhuA2Z66PQ"},
+const aliData = [{name:"名称：拉勾教育-测试开发实战训练营",url:"https://www.aliyundrive.com/s/GJ6ajVyP1nq"},
+{name:"名称：无与伦比的美丽 [更新至02集]",url:"https://www.aliyundrive.com/s/FBhuA2Z66PQ"},
 {name:"名称：惊天岳雷 [更新至12集]",url:"https://www.aliyundrive.com/s/dgwp2CdQXtX"},
 {name:"名称：夺宝奇兵5：命运转盘(2023) 4K HDR&amp;DV 外挂简中",url:"https://www.aliyundrive.com/s/hAWntVmXTbS"},
 {name:"名称：柜魔(2023) 4K HDR&amp;DV 外挂简中",url:"https://www.aliyundrive.com/s/c9rULwh9hjr"},
