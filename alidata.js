fcab0a27a59212355082cc79ebb57abfa1993a63
root <root@debian.debian>
2023-08-29 20:33:17 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：【大鹏教育】数据库MySQL课",url:"https://www.aliyundrive.com/s/igvB2Rf7kFW"},
+const aliData = [{name:"名称：Typora (Markdown 编辑器) v1.6.7 支持版本更新",url:"https://www.aliyundrive.com/s/6i5uANyY8r6"},
+{name:"名称：【大鹏教育】数据库MySQL课",url:"https://www.aliyundrive.com/s/igvB2Rf7kFW"},
 {name:"名称：一本书读懂ChatGPT、AIGC和元宇宙 | 电子书籍",url:"https://www.aliyundrive.com/s/VGVtTWmxi5R"},
 {name:"名称：夺宝奇兵5：命运转盘 (2023) 2160p HDR&amp;DV + 1080p 高码 DDP5.1 内封简英特效 &amp; 繁英特效双语【和谐不补】",url:"https://www.aliyundrive.com/s/ZmUEdso5okq"},
 {name:"名称：未来漫游指南 (2022) 纪录片 2160p 高码率 国语中字",url:"https://www.aliyundrive.com/s/rpjGmqhewN4"},
