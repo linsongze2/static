060a5a835bb524867b870bc46ab1783dd4939b12
root <root@debian.debian>
2023-08-30 01:41:17 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：荒野间谍 S01(2017) 1080P 中字外挂字幕",url:"https://www.aliyundrive.com/s/y42pTehSS1t"},
+const aliData = [{name:"名称：奥本海默 Oppenheimer (2023) 1080P 英语中字 豆瓣: 8.6",url:"https://www.aliyundrive.com/s/WiDgXMVnhv1"},
+{name:"名称：荒野间谍 S01(2017) 1080P 中字外挂字幕",url:"https://www.aliyundrive.com/s/y42pTehSS1t"},
 {name:"名称：无与伦比的美丽 [2023]芒臻[40集持续更新中] 更新4集",url:"https://www.aliyundrive.com/s/NvsmUVSFLRU"},
 {name:"名称：密室大逃脱 第五季 (2023) 持续更新",url:"https://www.aliyundrive.com/s/9H3JdgCR7pg"},
 {name:"名称：家务优等生 (2023) 持续更新",url:"https://www.aliyundrive.com/s/dUJd4ZZfXwC"},
