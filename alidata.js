ccd8cc1bdfa00fb1886d73eb66ba176368665f70
root <root@debian.debian>
2023-09-01 03:33:15 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：海贼王真人版 / 航海王 (2023) S01 2160p HDR NF 日英音轨 内封简繁英日 (内封双语版本今晚或明天) 【刮削】",url:"https://www.aliyundrive.com/s/5vn8RDH7FMx"},
+const aliData = [{name:"名称：披荆斩棘的哥哥3 (2023) 持续更新",url:"https://www.aliyundrive.com/s/PP6URfWfnpt"},
+{name:"名称：海贼王真人版 / 航海王 (2023) S01 2160p HDR NF 日英音轨 内封简繁英日 (内封双语版本今晚或明天) 【刮削】",url:"https://www.aliyundrive.com/s/5vn8RDH7FMx"},
 {name:"名称：零基础，30天，学会广告策划",url:"https://www.aliyundrive.com/s/Rd7wsGGr2m7"},
 {name:"名称：基地 Foundation (2023) S02 2160p HDR &amp; DV + 1080p 高码率 DDP5.1 内封简英特效 &amp; 繁英特效双语 更EP08",url:"https://www.aliyundrive.com/s/5Eip77Gqxqn"},
 {name:"名称：【海贼王 真人版 2023】1080P 高码率 内封简繁 日英双音轨 第一季已完结（八集全）",url:"https://www.aliyundrive.com/s/KZvSeeVc8TW"},
