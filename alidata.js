10c6c6fc5483c6e05126793d921a37353725681e
root <root@debian.debian>
2023-09-06 21:49:16 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：百妖谱 [1-3季]",url:"https://www.aliyundrive.com/s/wAWrMZiXeQd"},
+const aliData = [{name:"名称：没用的谎言 (2023) 热播韩剧 1080p 内封简繁 更E12【金所泫 / 黄旼炫】",url:"https://www.aliyundrive.com/s/u6BwSzGhzA3"},
+{name:"名称：百妖谱 [1-3季]",url:"https://www.aliyundrive.com/s/wAWrMZiXeQd"},
 {name:"名称：涂重点 小学语文1-6年级（上册）",url:"https://www.aliyundrive.com/s/tyxYvVnw87h"},
 {name:"名称：《零基础趣学Linux》学Linux入门必备",url:"https://www.aliyundrive.com/s/8QsG6UxKV5j"},
 {name:"名称：微专业 - Android开发工程师",url:"https://www.aliyundrive.com/s/3EkWTSdJuiS"},
