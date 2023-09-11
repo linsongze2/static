f8657bb4903b5ea29f9fb33829c9ca002596ef2b
root <root@debian.debian>
2023-09-10 21:57:18 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：【易锦】WEB渗透Hacker技术特训营（直播课第14期）",url:"https://www.aliyundrive.com/s/Po5fiVggtBP"},
+const aliData = [{name:"名称：25份研究分析报告（202309）",url:"https://www.aliyundrive.com/s/ggMwu3Jzy4K"},
+{name:"名称：【易锦】WEB渗透Hacker技术特训营（直播课第14期）",url:"https://www.aliyundrive.com/s/Po5fiVggtBP"},
 {name:"名称：2023年好书推荐：《公文写作标题结构速查宝典》",url:"https://www.aliyundrive.com/s/x5UbCxQcofX"},
 {name:"名称：我们诞生在中国 (2016) 纪录片 1080p BluRay 国英双音轨 内封多字幕",url:"https://www.aliyundrive.com/s/3gJE7ZbXsvh"},
 {name:"名称：《零浪费生活 》",url:"https://www.aliyundrive.com/s/99aQc5vAzKB"},
