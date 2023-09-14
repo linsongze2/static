eb5ca0872985ae36a1bb052e74fd4bed08654f36
root <root@debian.debian>
2023-09-14 07:25:17 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：异人之下 真人版 一人之下 (2023) 1080/4K HDR 更17",url:"https://www.aliyundrive.com/s/cbkUKsXkV1W"},
+const aliData = [{name:"名称：新男孩 The New Boy (2023)",url:"https://www.aliyundrive.com/s/8UJ2oqJvrrP"},
+{name:"名称：异人之下 真人版 一人之下 (2023) 1080/4K HDR 更17",url:"https://www.aliyundrive.com/s/cbkUKsXkV1W"},
 {name:"名称：西出玉门 [2023] 4K&amp;4K臻彩&amp;4K杜比更新16集",url:"https://www.aliyundrive.com/s/BjEzJbrsStt"},
 {name:"名称：花轿喜事 (2023) 1080/4K HDR 24集完结",url:"https://www.aliyundrive.com/s/jBGMqSuXyvJ"},
 {name:"名称：西出玉门 (2023) 1080/4K HDR 更16 倪妮 / 白宇",url:"https://www.aliyundrive.com/s/z6dvtvMx8Ko"},
