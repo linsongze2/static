cd2ca1f4c6faaa5f0ed5f79bc2d2aaa3ff148992
root <root@debian.debian>
2023-09-14 23:33:18 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：百炼成神 4K [更新至47集]」",url:"https://www.aliyundrive.com/s/rxGzCPu7hfM"},
+const aliData = [{name:"名称：基地(2021) 2季全 4K HDR 内封简繁",url:"https://www.aliyundrive.com/s/VarffgXhjSJ"},
+{name:"名称：百炼成神 4K [更新至47集]」",url:"https://www.aliyundrive.com/s/rxGzCPu7hfM"},
 {name:"名称：诱拐之日 (2023) 韩剧 1080p 中文字幕 更E02",url:"https://www.aliyundrive.com/s/co3zUqP9Xu3"},
 {name:"名称：国民死刑投票 (2023) 1080p AMZN 高码率 DDP2.0 内封多国语 热播韩剧漫改ip 更EP06【刮削】",url:"https://www.aliyundrive.com/s/42HJQKV3ibK"},
 {name:"名称：咒术回战 第二季 呪術廻戦 2 (2023) 1080/4K 更8 中字",url:"https://www.aliyundrive.com/s/DzdAUe1GaK5"},
