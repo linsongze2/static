8abed99287548ec0ade7f555fc68ed9cf40ee946
root <root@debian.debian>
2023-09-22 06:25:20 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：披荆斩棘的哥哥3 (2023)",url:"https://www.aliyundrive.com/s/DuvCJGxjWTr"},
+const aliData = [{name:"名称：西出玉门 (2023) 4K 臻彩&amp;4K 杜比 更新24",url:"https://www.aliyundrive.com/s/f1ooF9RtKLY"},
+{name:"名称：西出玉门 (2023) 1080/4K HDR 更24 倪妮 / 白宇",url:"https://www.aliyundrive.com/s/d2cRskBBFUD"},
+{name:"名称：披荆斩棘的哥哥3 (2023)",url:"https://www.aliyundrive.com/s/DuvCJGxjWTr"},
 {name:"名称：异人之下 真人版 一人之下 (2023) 1080/4K HDR 27集完结",url:"https://www.aliyundrive.com/s/2MQNxEZ2eL7"},
 {name:"名称：鬼吹灯.献王虫谷（2023）4K高码率 [60帧率版]",url:"https://www.aliyundrive.com/s/LGLPctaa7G7"},
 {name:"名称：关于我转生变成史莱姆这档事 S02 4K [全26集]",url:"https://www.aliyundrive.com/s/gpicBk3JXL7"},
