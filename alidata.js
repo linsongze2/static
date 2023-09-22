c7515e3c3343c827d2a24690f949eb1ed4d24124
root <root@debian.debian>
2023-09-22 09:09:28 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：铃芽之旅 4K原盘REMUX [HDR] [内封简日双字]",url:"https://www.aliyundrive.com/s/vkwpdqAyAy2"},
+const aliData = [{name:"名称：《未来水世界》4KREMUX [杜比视界] [国英双语] [内封简英双字+双语特效字幕]",url:"https://www.aliyundrive.com/s/Un8XZ2XM6u5"},
+{name:"名称：盗贼之歌 도적 칼의 소리 (2023) 9集完结",url:"https://www.aliyundrive.com/s/EperfJETRVc"},
+{name:"名称：铃芽之旅 4K原盘REMUX [HDR] [内封简日双字]",url:"https://www.aliyundrive.com/s/vkwpdqAyAy2"},
 {name:"名称：【拼客学院】2019年直播面授班 《全栈网络安全专家》 43期",url:"https://www.aliyundrive.com/s/UFwetD1kNTQ"},
 {name:"名称：邪不压正 (2018) 4K 中字硬字幕",url:"https://www.aliyundrive.com/s/q3HZ8aLTQ3Q"},
 {name:"名称：热带往事 (2021) 4K 60FPS 中英硬字幕",url:"https://www.aliyundrive.com/s/qeSCaBF7syA"},
