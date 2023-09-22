85f94378fa930890176ccd5dbcf538ca66f5d3f0
root <root@debian.debian>
2023-09-22 05:33:19 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：拆解一切故事写作 | 电子书籍",url:"https://www.aliyundrive.com/s/bkd3v3c4yNQ"},
+const aliData = [{name:"名称：【微师】安卓应用安全SO进阶培训班",url:"https://www.aliyundrive.com/s/aRAPoP2SMsK"},
+{name:"名称：拆解一切故事写作 | 电子书籍",url:"https://www.aliyundrive.com/s/bkd3v3c4yNQ"},
 {name:"名称：《人民画报》1950-1976 (323册)",url:"https://www.aliyundrive.com/s/BFuEczXhixx"},
 {name:"名称：赛马娘 S02 4K [全13集]",url:"https://www.aliyundrive.com/s/5ZeC6zqdnU6"},
 {name:"名称：献王虫谷 EDR高码杜比音",url:"https://www.aliyundrive.com/s/XeRVSVxedEt"},
