79add97046f15e94166d261df49991cc04bf1d39
root <root@debian.debian>
2023-09-22 22:25:17 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,7 @@
-const aliData = [{name:"名称：斗罗大陆2：绝世唐门 (2023) 4K 更15",url:"https://www.aliyundrive.com/s/3WwhigW1mJT"},
+const aliData = [{name:"名称：异人之下(2023) S01完结 4K",url:"https://www.aliyundrive.com/s/bzbi5FG3nXU"},
+{name:"名称：梅菲尔女巫 S01 [全8集][内封中字]",url:"https://www.aliyundrive.com/s/DAqLsH2rTUX"},
+{name:"名称：瞧这一家子(2010) S01 4K",url:"https://www.aliyundrive.com/s/v8HhdTbqns8"},
+{name:"名称：斗罗大陆2：绝世唐门 (2023) 4K 更15",url:"https://www.aliyundrive.com/s/3WwhigW1mJT"},
 {name:"名称：安乐传 (2023) 全39集 4K 【60帧率版本】",url:"https://www.aliyundrive.com/s/vHQYj5v2r1W"},
 {name:"名称：海雾 (2014) 韩版原盘REMUX [内封简繁字幕]",url:"https://www.aliyundrive.com/s/hQBQXL8aVHB"},
 {name:"名称：蓝甲虫 (2023) 2160p HDR &amp; DV 内封简英特效&amp;繁英特效双语 (人工翻译精校字幕) 【刮削】",url:"https://www.aliyundrive.com/s/chtb6UAwhsp"},
