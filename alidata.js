a15cda213cb2cd04928c41d2788ca3e31871b747
root <root@debian.debian>
2023-09-26 10:09:19 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：闪烁的西瓜 반짝이는 워터멜론 (2023)[更新至02集]",url:"https://www.aliyundrive.com/s/N3FLz8azoEM"},
+const aliData = [{name:"名称：送我上青云(2019) 4K高码",url:"https://www.aliyundrive.com/s/5pZ8q53LHon"},
+{name:"名称：闪烁的西瓜 반짝이는 워터멜론 (2023)[更新至02集]",url:"https://www.aliyundrive.com/s/N3FLz8azoEM"},
 {name:"名称：GT赛车：极速狂飙 (2023) 2160p / 1080p 高码 内封多国语",url:"https://www.aliyundrive.com/s/V9Lq4ArMAkF"},
 {name:"名称：九义人 (2023) 1080/4K HDR 完结 超前点映",url:"https://www.aliyundrive.com/s/qahwz7v32Hu"},
 {name:"名称：行尸走肉：达里尔·迪克森 (2023) 2160p 高码 DDP5.1 外挂双语 更E03",url:"https://www.aliyundrive.com/s/hUELqRPBgXS"},
