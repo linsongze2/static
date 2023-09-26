b5993bbfa9362903c5f23a94205de0ec1b4edeee
root <root@debian.debian>
2023-09-25 23:25:19 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：现代西方哲学十五讲 | 电子书籍",url:"https://www.aliyundrive.com/s/GtFHGBsfX8n"},
+const aliData = [{name:"名称：生化危机(2002) 4K原盘REMUX 国英音轨 特效字幕",url:"https://www.aliyundrive.com/s/FiRkWeYkkfJ"},
+{name:"名称：闪烁的西瓜 반짝이는 워터멜론 (2023)[更新至01集]",url:"https://www.aliyundrive.com/s/N3FLz8azoEM"},
+{name:"名称：现代西方哲学十五讲 | 电子书籍",url:"https://www.aliyundrive.com/s/GtFHGBsfX8n"},
 {name:"名称：【咕泡学院】P6ElasticStack高级开发与架构（实战班）- 2期",url:"https://www.aliyundrive.com/s/hP4KneYdKVt"},
 {name:"名称：美人记·昭君 (2023) 4K高码率 [60帧率版]",url:"https://www.aliyundrive.com/s/iAvXJwNqhQg"},
 {name:"名称：人生大事 (2022)✨【2160p.SDR】19.6G",url:"https://www.aliyundrive.com/s/ptoknGcSkva"},
