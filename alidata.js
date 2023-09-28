ef3427788c97ffdd83572b16184be80c3b614a06
root <root@debian.debian>
2023-09-28 10:57:19 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：时间的风景 (2012) 4K REMUX(蓝光 原盘)",url:"https://www.aliyundrive.com/s/Z7AWQGUiW1q"},
+const aliData = [{name:"名称：诱拐之日/绑架之日 [更新至06集]",url:"https://www.aliyundrive.com/s/qxAsnzTZFkn"},
+{name:"名称：时间的风景 (2012) 4K REMUX(蓝光 原盘)",url:"https://www.aliyundrive.com/s/Z7AWQGUiW1q"},
 {name:"名称：长安三万里 (2023) 1080P/4K 正式版",url:"https://www.aliyundrive.com/s/ECgUh4KdaaZ"},
 {name:"名称：高性能MySQL实战",url:"https://www.aliyundrive.com/s/o7v8H6yaZ2x"},
 {name:"名称：乐队的夏天 第三季 (2023)",url:"https://www.aliyundrive.com/s/cvhT5PnvRYo"},
