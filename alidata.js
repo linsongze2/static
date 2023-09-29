895be14bf95fad56d93419c31d78f81288b05170
root <root@debian.debian>
2023-09-29 03:01:19 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：半之半(2020) S01 4K 内封简繁",url:"https://www.aliyundrive.com/s/SRdznqwqwyh"},
+const aliData = [{name:"名称：你和我的警察课堂(2022) S01 1080p 内封简繁",url:"https://www.aliyundrive.com/s/RcztKBSfNiL"},
+{name:"名称：闪耀国度(2022) S01 4K HDR&amp;DV 内封简繁",url:"https://www.aliyundrive.com/s/Yn9veY5umMi"},
+{name:"名称：半之半(2020) S01 4K 内封简繁",url:"https://www.aliyundrive.com/s/SRdznqwqwyh"},
 {name:"名称：掠食城市(2018)4K REMUX",url:"https://www.aliyundrive.com/s/zAq92bFbaTR"},
 {name:"名称：6 Underground(2019) 4K 内封中字",url:"https://www.aliyundrive.com/s/EJAb2CkbwxU"},
 {name:"名称：足球尤物 (2006) 1080P 中字外挂字幕",url:"https://www.aliyundrive.com/s/dFaZ5ptk4cy"},
