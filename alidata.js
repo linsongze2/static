5283ee0592126fbff5b173e24f17496e24305cd6
root <root@debian.debian>
2023-10-02 22:25:22 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：情绪疗愈：摆脱内心的焦虑 | 电子书籍",url:"https://www.aliyundrive.com/s/X9bjX7NsD3T"},
+const aliData = [{name:"名称：闻香探案录 (2023) 24集完结",url:"https://www.aliyundrive.com/s/kuyfcm6jem1"},
+{name:"名称：情绪疗愈：摆脱内心的焦虑 | 电子书籍",url:"https://www.aliyundrive.com/s/X9bjX7NsD3T"},
 {name:"名称：修女(2018) 4K REMUX 外挂中字",url:"https://www.aliyundrive.com/s/sX5SrKUUa5C"},
 {name:"名称：天地逃生(2009) 4K REMUX 外挂中字",url:"https://www.aliyundrive.com/s/q8KkCneVJLw"},
 {name:"名称：红色星球(2000) 1080P REMUX 外挂中字",url:"https://www.aliyundrive.com/s/5u1uagFCM4D"},
