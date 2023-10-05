6b56de67bdf3b5da92550106ff7f662f039456b8
root <root@debian.debian>
2023-10-05 11:49:21 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：诱拐之日/绑架之日 [更新至07集]",url:"https://www.aliyundrive.com/s/qxAsnzTZFkn"},
+const aliData = [{name:"名称：恋爱不可抗力 이 연애는 불가항력 (2023)[更新至14集]",url:"https://www.aliyundrive.com/s/w4dVzrVbN43"},
+{name:"名称：诱拐之日/绑架之日 [更新至07集]",url:"https://www.aliyundrive.com/s/qxAsnzTZFkn"},
 {name:"名称：踏血寻梅 蓝光原盘REMUX [导演剪辑版] [内封简、繁中字]",url:"https://www.aliyundrive.com/s/vgpdqjb9obR"},
 {name:"名称：落日 (2023) 1080p 内封简繁 4集完结【悬疑日剧】",url:"https://www.aliyundrive.com/s/YRiJjor2GxX"},
 {name:"名称：意外 蓝光原盘REMUX [国粤双语] [内封简、繁中字]",url:"https://www.aliyundrive.com/s/DTgMtk7z4SC"},
