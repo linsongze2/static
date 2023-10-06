9788842c228542eafff020bf266de7c5095fa827
root <root@debian.debian>
2023-10-05 20:49:20 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：穷兄富弟 (2023) 4KHQ60FPS",url:"https://www.aliyundrive.com/s/twKHacjp8JX"},
+const aliData = [{name:"名称：好事成双 (2023) 2160p 国语中字 36集完结【黄晓明/张小斐】",url:"https://www.aliyundrive.com/s/qBsWp1dzdiY"},
+{name:"名称：保护我方城主大人(2023) S01 4K",url:"https://www.aliyundrive.com/s/mAYC5BcutGv"},
+{name:"名称：穷兄富弟 (2023) 4KHQ60FPS",url:"https://www.aliyundrive.com/s/twKHacjp8JX"},
 {name:"名称：伸冤人3 (2023) 2160p HDR&amp;DV MA 内封简英特效+繁英特效+粤英双语 【附1-2 4K HDR &amp; 4K REMUX】【刮削】",url:"https://www.aliyundrive.com/s/1HKSc1n1x1s"},
 {name:"名称：恋爱不可抗力 이 연애는 불가항력 (2023)[更新至14集]",url:"https://www.aliyundrive.com/s/w4dVzrVbN43"},
 {name:"名称：诱拐之日/绑架之日 [更新至07集]",url:"https://www.aliyundrive.com/s/qxAsnzTZFkn"},
