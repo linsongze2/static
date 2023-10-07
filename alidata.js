d19c9f2670337532ce339446884dfccdd4b44479
root <root@debian.debian>
2023-10-07 10:17:31 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：他从火光中走来(2023) S01 4K 内封简繁",url:"https://www.aliyundrive.com/s/KYkR5cEahRi"},
+const aliData = [{name:"名称：潘丘·维拉：北方的半人马 (2023) 2160p HDR10 内封多国语",url:"https://www.aliyundrive.com/s/pZ3MbFkCW7b"},
+{name:"名称：他从火光中走来(2023) S01 4K 内封简繁",url:"https://www.aliyundrive.com/s/KYkR5cEahRi"},
 {name:"名称：问心 4K&amp;杜比 首发5集",url:"https://www.aliyundrive.com/s/6o26HTBgWvJ"},
 {name:"名称：【国产剧】西出玉门[4K]",url:"https://www.aliyundrive.com/s/3BCTGwkKbXo"},
 {name:"名称：【国产剧】鹊刀门传奇[4K]",url:"https://www.aliyundrive.com/s/fGPvV74xZ2f"},
