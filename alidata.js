ed148479b64cb93bd53156569a93e39b8d7c87a7
root <root@debian.debian>
2023-10-06 23:01:21 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：恶魔法则",url:"https://www.aliyundrive.com/s/Vn6GhC5PtqZ"},
+const aliData = [{name:"名称：斗罗大陆2：绝世唐门 (2023) 4K 更17",url:"https://www.aliyundrive.com/s/aPiYKzCWLuu"},
+{name:"名称：恶魔法则",url:"https://www.aliyundrive.com/s/Vn6GhC5PtqZ"},
 {name:"名称：今天，你更博学了吗 | 电子书籍",url:"https://www.aliyundrive.com/s/CPc9KBfLyeY"},
 {name:"名称：星空 - Starfield 【游戏】",url:"https://www.aliyundrive.com/s/TdxjmHQZydb"},
 {name:"名称：大陆酒店(2023) S01完结 4K HDR&amp;DV 内封简繁",url:"https://www.aliyundrive.com/s/zDCHJZrfC4U"},
