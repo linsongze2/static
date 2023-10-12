644085c2c878c611d088c1e4c582ad7872787b31
root <root@debian.debian>
2023-10-12 01:49:19 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：小学初中数学和奥数资料（群友800买的）",url:"https://www.aliyundrive.com/s/jBDptKAUuvF"},
+const aliData = [{name:"名称：把思考作为习惯 | 电子书籍",url:"https://www.aliyundrive.com/s/LtTNo6khp4z"},
+{name:"名称：小学初中数学和奥数资料（群友800买的）",url:"https://www.aliyundrive.com/s/jBDptKAUuvF"},
 {name:"名称：电锯人 (2022) 1080P 中字外挂字幕",url:"https://www.aliyundrive.com/s/rmWoNQAVXNd"},
 {name:"名称：潘神的迷宫 (2006) 4K HDR 中字外挂字幕",url:"https://www.aliyundrive.com/s/stL9LaLmhf8"},
 {name:"名称：DolbyVision+HDR 杜比视界解码插件",url:"https://www.aliyundrive.com/s/4Xk7FQmcxL1"},
