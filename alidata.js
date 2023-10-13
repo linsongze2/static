3a411e417307f60ef925d795ba8bc828faf6ef4d
root <root@debian.debian>
2023-10-13 06:57:20 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：叠影狙击 4K高码 更新06 精彩继续",url:"https://www.aliyundrive.com/s/a7CSUFsUuJ7"},
+const aliData = [{name:"名称：乐队的夏天(2019) S03E19-E20(第10期上下) 4K",url:"https://www.aliyundrive.com/s/ByMHpvhgeXj"},
+{name:"名称：叠影狙击 4K高码 更新06 精彩继续",url:"https://www.aliyundrive.com/s/a7CSUFsUuJ7"},
 {name:"名称：少男少女恋爱 소년 소녀 연애하다 (2023)[更新至03集]",url:"https://www.aliyundrive.com/s/1imdbvVXqpH"},
 {name:"名称：交易 거래 (2023)[更新至04集]",url:"https://www.aliyundrive.com/s/sxzbouXbfXx"},
 {name:"名称：仙逆 [更新至05]",url:"https://www.aliyundrive.com/s/pKG4G462Jhq"},
