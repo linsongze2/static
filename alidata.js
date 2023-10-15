69c09b8959489a0d80d009ada1c6d70df945ab1e
root <root@debian.debian>
2023-10-15 07:09:20 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：还魂 2季全 1080P 中字内嵌字幕",url:"https://www.aliyundrive.com/s/TdDzRBNGKxV"},
+const aliData = [{name:"名称：田耕纪(2023) S01E01-E06 4K",url:"https://www.aliyundrive.com/s/NinWWwxmtgM"},
+{name:"名称：还魂 2季全 1080P 中字内嵌字幕",url:"https://www.aliyundrive.com/s/TdDzRBNGKxV"},
 {name:"名称：为有暗香来 (2023) 1080P 更9 周也/王星越",url:"https://www.aliyundrive.com/s/MpECPS4LJ1c"},
 {name:"名称：基地 S2(2023) 4K HDR 中字内嵌字幕",url:"https://www.aliyundrive.com/s/uHZusRSdDkJ"},
 {name:"名称：七人的逃脱 / 7人的逃脱 (2023) 1080p 高码率 内封简繁英 更EP08 【热播悬疑韩剧】【刮削】",url:"https://www.aliyundrive.com/s/BMSJd5Jgs4d"},
