00a97f70226c347db5b2cb8a75e9256937e240a5
root <root@debian.debian>
2023-10-16 08:09:22 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：我的助理不简单(2023) S01E01-E07 4K",url:"https://www.aliyundrive.com/s/YcwT5CfMjhU"},
+const aliData = [{name:"名称：田耕纪 [2023] 4K高码 更新08集",url:"https://www.aliyundrive.com/s/YNnq4eMwMgF"},
+{name:"名称：我的助理不简单(2023) S01E01-E07 4K",url:"https://www.aliyundrive.com/s/YcwT5CfMjhU"},
 {name:"名称：有生之年(2023) S01 1080P 内封简繁",url:"https://www.aliyundrive.com/s/xGa7ArEHhKv"},
 {name:"名称：虎鹤妖师录(2023) S01E01-E30 4K",url:"https://www.aliyundrive.com/s/L7eYcVA5nT8"},
 {name:"名称：叠影狙击 4K高码60帧 国粤双语 更新10集",url:"https://www.aliyundrive.com/s/vStfG3DD1et"},
