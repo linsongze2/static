dd9269a0bfd5865c7e969867beac0dee3c087fc4
root <root@debian.debian>
2023-10-20 05:33:20 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：【大讲台】基于大数据体系构建数据仓库",url:"https://www.aliyundrive.com/s/ar2A7AMtwy3"},
+const aliData = [{name:"名称：死神漫画全74卷.简体繁体日语三版本",url:"https://www.aliyundrive.com/s/8SRZHaKA7dU"},
+{name:"名称：【大讲台】基于大数据体系构建数据仓库",url:"https://www.aliyundrive.com/s/ar2A7AMtwy3"},
 {name:"名称：木偶惊魂 (2023) 4K 中字硬字幕",url:"https://www.aliyundrive.com/s/2sTxRVhwFsL"},
 {name:"名称：我的女神室友斗娜（2023）/官方中字",url:"https://www.aliyundrive.com/s/idk9aGyCa3k"},
 {name:"名称：天堂生存战(2023) S01 1080P 内封简繁",url:"https://www.aliyundrive.com/s/uiCG3RdtyTt"},
