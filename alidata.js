b907987d64e23d6b8b4ed6676a39198eba3e8ce6
root <root@debian.debian>
2023-10-26 08:41:21 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：心理学解压漫画 - 弗洛伊德与为什么鸭 | 电子书籍",url:"https://www.aliyundrive.com/s/YZoX7sTqsim"},
+const aliData = [{name:"名称：《真确》（扭转十大直觉偏误，发现事情比你想的美好）",url:"https://www.aliyundrive.com/s/KqihvdfSjwu"},
+{name:"名称：心理学解压漫画 - 弗洛伊德与为什么鸭 | 电子书籍",url:"https://www.aliyundrive.com/s/YZoX7sTqsim"},
 {name:"名称：王阳明大传-知行合一的心学智慧",url:"https://www.aliyundrive.com/s/hAZbcvqRP9D"},
 {name:"名称：叛我更幸福 (2023) 1080p NF DDP5.1 内封多国语 【刮削】",url:"https://www.aliyundrive.com/s/5GRGYBn3h1n"},
 {name:"名称：厄舍府的崩塌 The Fall of the House of Usher (2023) 8集全",url:"https://www.aliyundrive.com/s/x6f7F1VfFWL"},
