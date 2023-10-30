6e07ae6808d3f23e17b50ec22cafed01573b912b
root <root@debian.debian>
2023-10-30 06:09:21 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：零基础开发机器人应用项目班【图灵学院】",url:"https://www.aliyundrive.com/s/g61VSySnEz7"},
+const aliData = [{name:"名称：移动迷宫 (系列3部) 2160p HDR 国英音轨 内封特效 FRDS 蓝光版 动作 / 科幻【刮削】",url:"https://www.aliyundrive.com/s/Xts64ZL7enr"},
+{name:"名称：谁的问题—现代社会的非标准答案",url:"https://www.aliyundrive.com/s/LhPaRnE9fLo"},
+{name:"名称：零基础开发机器人应用项目班【图灵学院】",url:"https://www.aliyundrive.com/s/g61VSySnEz7"},
 {name:"名称：恐怖大师 S01-S02 1080p BluRay 高码 外挂简中 【刮削】",url:"https://www.aliyundrive.com/s/nQoEg3k7k46"},
 {name:"名称：思维是平的 | 电子书籍",url:"https://www.aliyundrive.com/s/aVPnbxxL4pF"},
 {name:"名称：哈萨克汗国：不败之剑 （蓝光原盘 REMUX...封装 ）（内嵌中字）（2017）",url:"https://www.aliyundrive.com/s/ZFyztnQg5GG"},
