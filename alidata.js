af0a6a3713ff3c7c8923f3485c798a8869918dac
root <root@debian.debian>
2023-10-30 01:17:28 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,7 @@
-const aliData = [{name:"名称：繁华似锦 (2023) 1080/4K HDR 38集完结 张雨绮/左小青",url:"https://www.aliyundrive.com/s/qMvroXoYLXb"},
+const aliData = [{name:"名称：幽灵公主 もののけ姫 (1997) H265.1080P.国日双语.中日特效字幕",url:"https://www.aliyundrive.com/s/ieSQaF4n1MR"},
+{name:"名称：止痛骗 Pain Hustlers (2023) 1080p NF 高码 内封多国语【犯罪片】",url:"https://www.aliyundrive.com/s/iAsvS3mmPLc"},
+{name:"名称：拆案：黎明将至 4KHDR60FPS 更新17集",url:"https://www.aliyundrive.com/s/HvADc7zxMsp"},
+{name:"名称：繁华似锦 (2023) 1080/4K HDR 38集完结 张雨绮/左小青",url:"https://www.aliyundrive.com/s/qMvroXoYLXb"},
 {name:"名称：恐惧暗夜 (2023) 4K REMUX 原盘 HDR 外挂双语 【刮削】",url:"https://www.aliyundrive.com/s/jgc3PKVxGMm"},
 {name:"名称：仙逆 4K 更新08集",url:"https://www.aliyundrive.com/s/PuwNNHupb4F"},
 {name:"名称：刺客俱乐部 (2023)✨【2160p.HDR】【原轨.高码率】12G",url:"https://www.aliyundrive.com/s/jfcT7k3SXD4"},
