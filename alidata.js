daf8d3f5ab0fc44abbf3641a9331da91bfc42dfe
root <root@debian.debian>
2023-10-29 20:49:21 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：《漫画算法：小灰的算法之旅》",url:"https://www.aliyundrive.com/s/b3b5xKhWFbM"},
+const aliData = [{name:"名称：再造战士(1992) 4K PROPER REMUX 外挂中字",url:"https://www.aliyundrive.com/s/KbBgNRWPe8V"},
+{name:"名称：信使(2020) 4K REMUX 外挂中字",url:"https://www.aliyundrive.com/s/gsrbXDRP8pE"},
+{name:"名称：《漫画算法：小灰的算法之旅》",url:"https://www.aliyundrive.com/s/b3b5xKhWFbM"},
 {name:"名称：芯片战争：世界最关键技术的争夺战",url:"https://www.aliyundrive.com/s/ow4xjomAhbm"},
 {name:"名称：神盾局特工 Agents of S.H.I.E.L.D.系列 1~7季全",url:"https://www.aliyundrive.com/s/WpFop1R95sL"},
 {name:"名称：看人的艺术：11种以物识人术",url:"https://www.aliyundrive.com/s/EQzGZbTSuoN"},
