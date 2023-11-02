142fdaad1e01de4c2a02d4cc7c280ccc86c3a5ce
root <root@debian.debian>
2023-11-02 19:41:22 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：斯巴达克斯 1-3季 无删减 高码率1080P 内封中英特效字幕【易河蟹，速存】",url:"https://www.aliyundrive.com/s/sG8rS955jyn"},
+const aliData = [{name:"名称：中信十年经典：大历史十书（套装共10册）",url:"https://www.aliyundrive.com/s/DDBGgFXM8kk"},
+{name:"名称：斯巴达克斯 1-3季 无删减 高码率1080P 内封中英特效字幕【易河蟹，速存】",url:"https://www.aliyundrive.com/s/sG8rS955jyn"},
 {name:"名称：Three.js企业3D可视化实战项目WEBGL系统体系课程",url:"https://www.aliyundrive.com/s/nacE3c8UKBH"},
 {name:"名称：克洛伊(2010) 1080P REMUX 外挂中字 易和谐",url:"https://www.aliyundrive.com/s/TpcZybUq2bK"},
 {name:"名称：缩小人生(2017) 4K REMUX 外挂中字",url:"https://www.aliyundrive.com/s/p6WTCeFyBpS"},
