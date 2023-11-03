b0ae9b2b2f967d78b3d24d75a7a619abf345fd39
root <root@debian.debian>
2023-11-03 07:01:23 -0400

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：隐秘而伟大 (2020)✨【2160p.SDR】【原轨.高画质】720M/集",url:"https://www.aliyundrive.com/s/g2nia2bztHu"},
+const aliData = [{name:"名称：《博弈论：每个人都能成为决策高手》 【EPUB | MOBI | PDF 电子书】",url:"https://www.aliyundrive.com/s/NccmeCqETR3"},
+{name:"名称：风筝 (2017)✨【2160p.SDR】【原轨.高画质】1.2G/集",url:"https://www.aliyundrive.com/s/Asz5hcmrK1t"},
+{name:"名称：隐秘而伟大 (2020)✨【2160p.SDR】【原轨.高画质】720M/集",url:"https://www.aliyundrive.com/s/g2nia2bztHu"},
 {name:"名称：拆案：黎明将至 (2023) 1080/4K 23集完结 董璇",url:"https://www.aliyundrive.com/s/o4tJGS7kxBJ"},
 {name:"名称：第八个嫌疑人 (2023)4K60FPS&amp;4K.HQ.DDP5.1",url:"https://www.aliyundrive.com/s/HArSsvpKY8x"},
 {name:"名称：故乡，别来无恙 (2023) 首更4 任素汐/李雪琴/史策",url:"https://www.aliyundrive.com/s/CGbzd4k5Tqk"},
