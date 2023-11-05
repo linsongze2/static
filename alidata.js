3c0413c3377ac88bb8746e4b6257da9c91c14296
root <root@debian.debian>
2023-11-05 02:25:24 -0500

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：【尚硅谷】Java安全密码学详解",url:"https://www.aliyundrive.com/s/RSqGuJenmmC"},
+const aliData = [{name:"名称：史泰龙的传奇 Sly (2023) 1080p DDP5.1 NF 内封多国语 【纪录片】【刮削】",url:"https://www.aliyundrive.com/s/FhNbjVCF2uz"},
+{name:"名称：【尚硅谷】Java安全密码学详解",url:"https://www.aliyundrive.com/s/RSqGuJenmmC"},
 {name:"名称：丧尸之城 Zombie Town (2023) 1080p 机翻双语 恐怖片【刮削】",url:"https://www.aliyundrive.com/s/zqf2sq7dWnh"},
 {name:"名称：有滋有味内蒙古(2021) S01 4K",url:"https://www.aliyundrive.com/s/BJtHw56qz6Y"},
 {name:"名称：雷蒙·斯尼奇的不幸历险 (2004) 1080P 中英外挂字幕",url:"https://www.aliyundrive.com/s/isjd47fChiN"},
