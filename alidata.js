8796cca0042fb9784be5ab05bf3f82835d2e1345
root <root@debian.debian>
2023-11-06 10:33:21 -0500

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：珍的不一样 (2023)✨【2160p.HDR】【兼容杜比视界】【原轨.高画质】2.7G/集",url:"https://www.aliyundrive.com/s/GmLCSQFGEgz"},
+const aliData = [{name:"名称：后翼弃兵 (2020)✨【2160p.HDR】【原轨.高码率】7.1G/集",url:"https://www.aliyundrive.com/s/DYxeWKYTvdZ"},
+{name:"名称：蝴蝶效应 3部 1080P 国英双音轨 默认英语 中英外挂字幕",url:"https://www.aliyundrive.com/s/dqqYeSzJhMx"},
+{name:"名称：珍的不一样 (2023)✨【2160p.HDR】【兼容杜比视界】【原轨.高画质】2.7G/集",url:"https://www.aliyundrive.com/s/GmLCSQFGEgz"},
 {name:"名称：医疗的背后：那些关于生命、健康和医疗的真相",url:"https://www.aliyundrive.com/s/ZZVBNVQ26G3"},
 {name:"名称：顾问 (2023)✨【1080p.SDR】【硬字幕】820M/集",url:"https://www.aliyundrive.com/s/dgnaFgyKYF9"},
 {name:"名称：婚礼大捷 혼례대첩 (2023) 更3",url:"https://www.aliyundrive.com/s/XohVKr6DTki"},
