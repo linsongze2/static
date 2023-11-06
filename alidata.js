ad74fa0a452f8b1e2e5d79dbe187982f02a61754
root <root@debian.debian>
2023-11-05 20:09:22 -0500

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：好书推荐：《价值投资与常赢之道（套装共10册）》",url:"https://www.aliyundrive.com/s/AEZ1jRRaPzr"},
+const aliData = [{name:"名称：匹诺曹 피노키오 (2014)✨【1080p.SDR】【字幕组版】2G/集",url:"https://www.aliyundrive.com/s/Em9UqgKN64E"},
+{name:"名称：好书推荐：《价值投资与常赢之道（套装共10册）》",url:"https://www.aliyundrive.com/s/AEZ1jRRaPzr"},
 {name:"名称：我记得(2022) 1080P 内封中字",url:"https://www.aliyundrive.com/s/TBWC19oW4nV"},
 {name:"名称：七人的逃脱 7인의 탈출 (2023) 1080p 中文字幕 更E14【悬疑韩剧】",url:"https://www.aliyundrive.com/s/3gTfhZSwAAg"},
 {name:"名称：种群 (2023)✨【1080p.SDR】【原轨.高码率】2G/集",url:"https://www.aliyundrive.com/s/MB4xsQUKd5d"},
