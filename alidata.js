ae16b863bf109425d0499dd4c62427116af4ba1b
root <root@debian.debian>
2023-11-14 09:32:40 -0500

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：罪孽深种 Criminal Code (2023) S01 1080p NF 高码 DDP5.1 内封多国语 【巴西犯罪片】【刮削】",url:"https://www.aliyundrive.com/s/tEzrRVjvepX"},
+const aliData = [{name:"名称：婚礼大捷 혼례대첩 (2023)[更新至06集]",url:"https://www.aliyundrive.com/s/8xfkh47DJPd"},
+{name:"名称：罪孽深种 Criminal Code (2023) S01 1080p NF 高码 DDP5.1 内封多国语 【巴西犯罪片】【刮削】",url:"https://www.aliyundrive.com/s/tEzrRVjvepX"},
 {name:"名称：博很恐惧 (2023)✨【2160p.HDR】【兼容杜比视界】【原轨.高码率】32G",url:"https://www.aliyundrive.com/s/DRoeNYzq9n2"},
 {name:"名称：Java从单体到微服务打造房产销售平台",url:"https://www.aliyundrive.com/s/pSg6jnpV4EB"},
 {name:"名称：AI创世者 The Creator (2023) 1080/4K 中英字幕",url:"https://www.aliyundrive.com/s/hUaacUpT4sL"},
