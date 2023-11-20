af65b1154f55c551fae440b8fa2c9f0aa1c757bd
root <root@debian.debian>
2023-11-20 07:40:39 -0500

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：杀手之王 蓝光原盘REMUX [英版 官方画质修复] [国粤双语] [内封简、繁中字]",url:"https://www.aliyundrive.com/s/Vjw5N9rAxga"},
+const aliData = [{name:"名称：超人 (1978) 4K BD 内封中字",url:"https://www.aliyundrive.com/s/oDjvRMsXy6P"},
+{name:"名称：杀手之王 蓝光原盘REMUX [英版 官方画质修复] [国粤双语] [内封简、繁中字]",url:"https://www.aliyundrive.com/s/Vjw5N9rAxga"},
 {name:"名称：剧集更新",url:"https://www.aliyundrive.com/s/5sbaSB3s5H6"},
 {name:"名称：无所畏惧[2023] 4KHDR60FPS（40集完结）",url:"https://www.aliyundrive.com/s/Pdp4y7HHtFc"},
 {name:"名称：无所畏惧 (2023) 1080/4K HDR 40集完结 热依扎/王阳",url:"https://www.aliyundrive.com/s/FkpuFjpNAJN"},
