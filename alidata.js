b0fb81725b72cf4afd7dd781022dcc572980a5fc
root <root@debian.debian>
2023-11-21 07:56:39 -0500

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,6 @@
-const aliData = [{name:"名称：恋人 [全21集][内封简繁字幕]",url:"https://www.aliyundrive.com/s/8xhHCDa1ui3"},
+const aliData = [{name:"名称：斗破苍穹·觉醒 (2023) 4K",url:"https://www.aliyundrive.com/s/y5QNviDmQvX"},
+{name:"名称：以爱为营 (2023) 1080/4K HDR 更30 白鹿/王鹤棣",url:"https://www.aliyundrive.com/s/VbvrBgyKoVk"},
+{name:"名称：恋人 [全21集][内封简繁字幕]",url:"https://www.aliyundrive.com/s/8xhHCDa1ui3"},
 {name:"名称：剧集更新",url:"https://www.aliyundrive.com/s/DRvK7A7igJt"},
 {name:"名称：100万个约定之宁安如梦 (2023) 更3",url:"https://www.aliyundrive.com/s/yjqafnNP4g7"},
 {name:"名称：樊登读书会 2023年更新1118 修复玻璃心",url:"https://www.aliyundrive.com/s/qHp5YYyKXnd"},
