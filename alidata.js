2771c451b9ed2712cf265fe176b5f04e9439d147
root <root@debian.debian>
2023-11-23 07:24:38 -0500

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：小飞龙 (1972) 720P 国语",url:"https://www.aliyundrive.com/s/nPWGxAaiovY"},
+const aliData = [{name:"名称：迈克尔·杰克逊.颤栗 (1983) 4K 官方修复",url:"https://www.aliyundrive.com/s/U99wsWpwUKJ"},
+{name:"名称：小飞龙 (1972) 720P 国语",url:"https://www.aliyundrive.com/s/nPWGxAaiovY"},
 {name:"名称：12勇士 (2018) 4K REMUX 外挂中字",url:"https://www.aliyundrive.com/s/wa12ep4ht4k"},
 {name:"名称：宁安如梦 4K.HDR.60FPS.DDP5.1 更新30集",url:"https://www.aliyundrive.com/s/qkonoPqTrZa"},
 {name:"名称：冰血暴 Fargo (2014) 五季全✨【1080p/2160p.SDR】【原轨.高码率】在更",url:"https://www.aliyundrive.com/s/LU6gP9mpw3Y"},
