7c5d05b0d2999b82373255925a4fc5d7b252b155
root <root@debian.debian>
2023-11-24 09:48:37 -0500

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：迷失在世界尽头-漫画",url:"https://www.aliyundrive.com/s/3ayXRtWfV1p"},
+const aliData = [{name:"名称：007：大战皇家赌场 (2006) 4K REMUX 内封中字",url:"https://www.aliyundrive.com/s/n1oS2VJAAX4"},
+{name:"名称：迷失在世界尽头-漫画",url:"https://www.aliyundrive.com/s/3ayXRtWfV1p"},
 {name:"名称：瞬息全宇宙 4K HDR",url:"https://www.aliyundrive.com/s/c3aZUJiNriG"},
 {name:"名称：猪猪侠（1～16部合集）",url:"https://www.aliyundrive.com/s/eoQR7CoH7Qn"},
 {name:"名称：帝陵·西汉帝陵 (2015) 4K",url:"https://www.aliyundrive.com/s/XtUBihRdVwV"},
