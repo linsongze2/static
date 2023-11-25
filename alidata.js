bc945287a8b96487818f7971cade4573352eaf71
root <root@debian.debian>
2023-11-25 03:16:37 -0500

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：撞邪 4KHQ60FPS",url:"https://www.aliyundrive.com/s/nCP1NjJzw1J"},
+const aliData = [{name:"名称：种豆得豆 콩콩팥팥 (2023)[更新至07集]",url:"https://www.aliyundrive.com/s/hcfdSaDU2tm"},
+{name:"名称：撞邪 4KHQ60FPS",url:"https://www.aliyundrive.com/s/nCP1NjJzw1J"},
 {name:"名称：赛博朋克：边缘行者 日英双音轨+35种多语言字幕",url:"https://www.aliyundrive.com/s/gwieevdvAfv"},
 {name:"名称：凡人修仙传：星海飞驰篇 凡人修仙传星海飞驰篇 (2023) 4K 更77",url:"https://www.aliyundrive.com/s/sANUufry1Fz"},
 {name:"名称：不死不幸",url:"https://www.aliyundrive.com/s/j3t1jxJJXMC"},
