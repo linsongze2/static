a6c8649322b08c8ad88d89fc88edc4d787f029e5
root <root@debian.debian>
2023-12-06 08:56:40 -0500

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,7 @@
-const aliData = [{name:"名称：地球脉动 Planet Earth (2006) 三季全✨【2160p.HDR】【附国配版】在更E07【补档】",url:"https://www.aliyundrive.com/s/C6hZFikMWaK"},
+const aliData = [{name:"名称：鹿鼎记 蓝光原盘REMUX [高码英版 官方画质修复] [国粤双语] [内封简英双字]",url:"https://www.aliyundrive.com/s/3wt81pQrsgr"},
+{name:"名称：今天也很可爱的狗 오늘도 사랑스럽개 (2023)[更新至09集]",url:"https://www.aliyundrive.com/s/n6rCTpWuibr"},
+{name:"名称：鹿鼎记2：神龙教 蓝光原盘REMUX [高码英版 官方画质修复] [国粤双语] [内封简、繁中字]",url:"https://www.aliyundrive.com/s/tzyXBM9q31K"},
+{name:"名称：地球脉动 Planet Earth (2006) 三季全✨【2160p.HDR】【附国配版】在更E07【补档】",url:"https://www.aliyundrive.com/s/C6hZFikMWaK"},
 {name:"名称：无人岛的Diva 무인도의 디바 (2023) 1080p NF 内封多国语 12集完结",url:"https://www.aliyundrive.com/s/4b2Sv4gLkTL"},
 {name:"名称：地球脉动 Planet Earth (2006) 三季全✨【2160p.HDR】【附国配版】在更E07",url:"https://www.aliyundrive.com/s/UBNuU33XJJH"},
 {name:"名称：一念关山 (2023)✨【2160p.HDR】【原轨.高码率】4.5G/集 更E18【补档速存】",url:"https://www.aliyundrive.com/s/hLxchNgka88"},
