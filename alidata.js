93b4043b324c348ba4e5921ea92681db56ba4c46
root <root@debian.debian>
2023-12-09 07:16:41 -0500

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：一念关山 4K.HDR.60FPS.DDP5.1 更新24集",url:"https://www.aliyundrive.com/s/uaqXAzSmtnL"},
+const aliData = [{name:"名称：逆天邪神 4K高码 更新14集",url:"https://www.aliyundrive.com/s/C4dL14V95bm"},
+{name:"名称：一念关山 4K.HDR.60FPS.DDP5.1 更新24集",url:"https://www.aliyundrive.com/s/uaqXAzSmtnL"},
 {name:"名称：我的男人是丘比特 내 남자는 큐피드 (2023)[更新至04集]",url:"https://www.aliyundrive.com/s/8KV2iG6cosL"},
 {name:"名称：凡人修仙传 新年番 (2023) 4K 更79",url:"https://www.aliyundrive.com/s/DSXF7VxyyS9"},
 {name:"名称：种豆得豆 콩콩팥팥 (2023)[全9集]",url:"https://www.aliyundrive.com/s/hcfdSaDU2tm"},
