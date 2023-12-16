e82a8aae71b062a3fa5cf6ba5e110c5026c7daf5
root <root@debian.debian>
2023-12-15 20:32:42 -0500

    -

--- alidata.js
+++ alidata.js
@@ -1,4 +1,5 @@
-const aliData = [{name:"名称：咒术回战 第二季 4K 内封字幕",url:"https://www.aliyundrive.com/s/eSKhKjdj5sn"},
+const aliData = [{name:"名称：帝王计划：怪兽遗产(2023)4K HDR S01—E06",url:"https://www.aliyundrive.com/s/QzhUx11vGz6"},
+{name:"名称：咒术回战 第二季 4K 内封字幕",url:"https://www.aliyundrive.com/s/eSKhKjdj5sn"},
 {name:"名称：火星情报局 第六季[2023] 4K 更新最新",url:"https://www.aliyundrive.com/s/4zdC5YMTkVe"},
 {name:"名称：烈女朴氏契约结婚传 열녀박씨 계약결혼뎐 (2023)[更新至07集]",url:"https://www.aliyundrive.com/s/YKgAWdjgC8W"},
 {name:"名称：与恶魔有约 마이데몬 (2023) [更新至07集]",url:"https://www.aliyundrive.com/s/3c6yf1J3G8K"},
